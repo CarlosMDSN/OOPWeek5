127d292872d21387126c551400dd8c5db9a661b9
CARLOSMOREIRADOSSANT <G00429041@atu.ie>
2024-10-16 16:54:04 +0100

    Step 5

---
+++ src/main/java/ie/atu/week4/ArrayDemo.java
@@ -0,0 +1,28 @@
+package ie.atu.week4;
+
+import java.util.ArrayList;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+       int [] anArray;
+       int i;
+
+       anArray = new int[10];
+
+       anArray[0] = 100;
+       anArray[1] = 200;
+       anArray[2] = 300;
+       anArray[3] = 400;
+       anArray[4] = 500;
+       anArray[5] = 600;
+       anArray[6] = 700;
+       anArray[7] = 800;
+       anArray[8] = 900;
+       anArray[9] = 1000;
+
+        for(i = 0; i<10; i++)
+        {
+            System.out.println("The value is " + anArray[i]);
+        }
+    }
+}
